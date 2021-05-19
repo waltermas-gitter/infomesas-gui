4ae576c8280c37a06d3ab2b81b323fbf2a90ec2a
waltermas <walterjmas@gmail.com>
2021-05-19 17:30:59 -0300

    probando ventanas

--- infomesas-gui.py
+++ infomesas-gui.py
@@ -3,8 +3,8 @@
 # https://realpython.com/python-pyqt-database/
 
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
-from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QMessageBox, QTableWidgetItem
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
 import sys
 
 # Create the connection
@@ -19,6 +19,14 @@ if not con.open():
         "Database Error: %s" % con.lastError().databaseText(),
     )
     sys.exit(1)
+    
+
+class PedidoWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("pedido.ui", self)
+
+
 
 class InfomesasWindow(QMainWindow):
     def __init__(self):
@@ -29,6 +37,24 @@ class InfomesasWindow(QMainWindow):
 
     def initUI(self):
         self.actionSalir.triggered.connect(self.salir)
+        self.pushButton.clicked.connect(self.editarPedido)
+
+        self.pedidosTableWidget.setColumnCount(4)
+        self.pedidosTableWidget.setHorizontalHeaderLabels(["ID", "Name", "Job", "Email"])
+        query = QSqlQuery("SELECT idPedido, fecha, cliente, modelo FROM pedidos")
+        while query.next():        
+            rows = self.pedidosTableWidget.rowCount()
+            self.pedidosTableWidget.setRowCount(rows + 1)
+            self.pedidosTableWidget.setItem(rows, 0, QTableWidgetItem(str(query.value(0))))
+            self.pedidosTableWidget.setItem(rows, 1, QTableWidgetItem(query.value(1)))
+            self.pedidosTableWidget.setItem(rows, 2, QTableWidgetItem(str(query.value(2))))
+            self.pedidosTableWidget.setItem(rows, 3, QTableWidgetItem(query.value(3)))
+        self.pedidosTableWidget.resizeColumnsToContents()                    
+
+    def editarPedido(self):
+        self.pedido = PedidoWindow()
+        print(self.pedido)
+        self.pedido.show()
 
     def salir(self):
         con.close()
@@ -40,6 +66,7 @@ def window():
     app = QApplication(sys.argv)
     win = InfomesasWindow()
     win.show()
+    
     sys.exit(app.exec_())
 
 window()
