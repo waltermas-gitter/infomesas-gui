0076411990e771cbcda3cb7537e1c8e82cc71937
waltermas <walterjmas@gmail.com>
2021-05-21 18:07:38 -0300

    dateando qtable

--- infomesas-gui.py
+++ infomesas-gui.py
@@ -13,6 +13,8 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtSql import *
 from PyQt5 import uic
+from datetime import datetime, timedelta
+
 
 # Create the connection
 con = QSqlDatabase.addDatabase("QSQLITE")
@@ -48,16 +50,39 @@ class InfomesasWindow(QMainWindow):
         self.actionSalir.triggered.connect(self.salir)
         self.pushButton.clicked.connect(self.editarPedido)
 
-        self.pedidosTableWidget.setColumnCount(4)
-        self.pedidosTableWidget.setHorizontalHeaderLabels(["ID", "Name", "Job", "Email"])
-        query = QSqlQuery("SELECT idPedido, fecha, cliente, modelo FROM pedidos")
+        # lleno pedidosTableWidget
+        self.pedidosTableWidget.setColumnCount(13)
+        self.pedidosTableWidget.setHorizontalHeaderLabels(["ID", "Fecha", "Cliente", "Modelo", "Chapa", "Notas", "M.cerrada", "M.abierta", "M.ancho", "Precio", "Estado", "F.entrega", "Lugar entrega"])
+        query = QSqlQuery("SELECT * FROM pedidos")
         while query.next():        
             rows = self.pedidosTableWidget.rowCount()
             self.pedidosTableWidget.setRowCount(rows + 1)
             self.pedidosTableWidget.setItem(rows, 0, QTableWidgetItem(str(query.value(0))))
-            self.pedidosTableWidget.setItem(rows, 1, QTableWidgetItem(query.value(1)))
-            self.pedidosTableWidget.setItem(rows, 2, QTableWidgetItem(str(query.value(2))))
+            fecha = datetime.strptime(query.value(1), "%Y-%m-%d %H:%M:%S")
+            # fechap = "%s-%s-%s" % (fecha.day, fecha.month, fecha.year)
+            fechap = fecha.strftime("%d-%m-%Y")
+            
+            self.pedidosTableWidget.setItem(rows, 1, QTableWidgetItem(fechap))
+            # cur.execute("SELECT nombre from clientes WHERE idCliente = %s" % (item[2])) 
+            # cliente = cur.fetchall()[0][0] 
+            queryCliente = QSqlQuery("SELECT nombre FROM clientes WHERE idCliente = %s" % query.value(2))
+            queryCliente.first()
+ 
+            self.pedidosTableWidget.setItem(rows, 2, QTableWidgetItem(str(queryCliente.value(0))))
+
+
+
             self.pedidosTableWidget.setItem(rows, 3, QTableWidgetItem(query.value(3)))
+            self.pedidosTableWidget.setItem(rows, 4, QTableWidgetItem(str(query.value(4))))
+            self.pedidosTableWidget.setItem(rows, 5, QTableWidgetItem(str(query.value(5))))
+            self.pedidosTableWidget.setItem(rows, 6, QTableWidgetItem(str(query.value(6))))
+            self.pedidosTableWidget.setItem(rows, 7, QTableWidgetItem(str(query.value(7))))
+            self.pedidosTableWidget.setItem(rows, 8, QTableWidgetItem(str(query.value(8))))
+            self.pedidosTableWidget.setItem(rows, 9, QTableWidgetItem(str(query.value(9))))
+            self.pedidosTableWidget.setItem(rows, 10, QTableWidgetItem(str(query.value(10))))
+            self.pedidosTableWidget.setItem(rows, 11, QTableWidgetItem(str(query.value(11))))
+            self.pedidosTableWidget.setItem(rows, 12, QTableWidgetItem(str(query.value(12))))
+
         self.pedidosTableWidget.resizeColumnsToContents()                    
 
     def editarPedido(self):
