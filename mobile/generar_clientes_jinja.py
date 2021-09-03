e3c92b93a3d364aeec9131905d3fe180b011e29a
waltermas <walterjmas@gmail.com>
2021-09-03 12:10:28 -0300

    vie 03 sep 2021 12:10:28 -03

--- mobile/generar_clientes_jinja.py
+++ mobile/generar_clientes_jinja.py
@@ -6,19 +6,21 @@ import os
 from jinja2 import Template
 
 def main():
-    # jinja2_template_string = open("pedidos_template.html", 'r').read()
-    # template = Template(jinja2_template_string)
+    jinja2_template_string = open("clientes_template.html", 'r').read()
+    template = Template(jinja2_template_string)
     conn = sqlite3.connect('../infomesas.db')
     cur = conn.cursor()
     cur.execute("SELECT * from clientes ORDER BY nombre")
+    clientes = []
     data = cur.fetchall()
     for item in data:
+        clientes.append(item[1])
         print(item[1])
 
-    # html_template_string = template.render(pedidos=pedidos)
+    html_template_string = template.render(clientes=clientes)
     # print(html_template_string)
 
-    # pedidos_file = open("pedidos.html", 'w').write(html_template_string)
+    template_file = open("clientes.html", 'w').write(html_template_string)
 
 if __name__ == '__main__':
     main()
