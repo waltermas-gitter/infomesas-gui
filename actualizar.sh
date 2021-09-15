4b3b59dc70e2aa6eb9f09ff7c00314a43dc6d03b
waltermas <walterjmas@gmail.com>
2021-09-15 18:48:37 -0300

    mié 15 sep 2021 18:48:37 -03

--- actualizar.sh
+++ actualizar.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+TIMEFORMAT='%3U'
 cd /home/waltermas/MEGAsync/scripts/infomesas-gui/mobile
-poetry run python m_generar_pendientes_jinja.py
-poetry run python m_generar_cada_cliente_jinja.py
-poetry run python m_generar_todos_pedidos_jinja.py
+echo -n "generar pendientes mobile "
+time poetry run python m_generar_pendientes_jinja.py 
+echo -n "generar cada cliente mobile "
+time poetry run python m_generar_cada_cliente_jinja.py
+echo -n "generar todos los pedidos mobile "
+time poetry run python m_generar_todos_pedidos_jinja.py
 
 cd /home/waltermas/MEGAsync/scripts/infomesas-gui
 poetry run python generar_cheques_jinja.py
@@ -20,3 +24,4 @@ message=`date`
 git commit -m "$message"
 git push
 notify-send "infomesas actualizado con exito"
+read -n1 -s -r -p $'Press any key to continue...\n' key
