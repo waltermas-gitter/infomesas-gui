6333739c629f0cb3a040e91c4836f3eeea4c2adb
waltermas <walterjmas@gmail.com>
2021-09-23 18:48:47 -0300

    jue 23 sep 2021 18:48:47 -03

---
+++ web/generar_clientes_jinja.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+# import sqlite3
+# from datetime import datetime, timedelta
+import os
+
+from jinja2 import Template, FileSystemLoader, Environment
+# from coloresClientes import devuelvoColorCliente
+
+env = Environment()
+env.loader = FileSystemLoader('.')
+
+def main():
+    # jinja2_template_string = open("clientes_template.html", 'r').read()
+    # template = Template(jinja2_template_string)
+    # conn = sqlite3.connect('../infomesas.db')
+    # cur = conn.cursor()
+    # cur.execute("SELECT * from clientes ORDER BY nombre")
+    # clientes = []
+    # data = cur.fetchall()
+    # for item in data:
+    #     ofuscado = codecs.encode(item[1].replace(' ', '-'), 'rot_13')
+    #     clientes.append((item[1], "https://waltermas-gitter.github.io/infomesas-gui/mobile/%s.html" % ofuscado, devuelvoColorCliente(item[1])))
+    #     # os.system("rm %s.html" % item[1].replace(' ', '-'))
+
+    tmpl = env.get_template('clientes_template.html')
+    html_template_string = tmpl.render()
+    template_file = open("clientes.html", 'w').write(html_template_string)
+
+if __name__ == '__main__':
+    main()
