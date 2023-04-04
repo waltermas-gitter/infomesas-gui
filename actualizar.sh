3820e6df00239a717b2007d5fc8befba24808cc7
waltermas <walterjmas@gmail.com>
2023-04-04 18:46:08 -0300

    mar 04 abr 2023 18:46:08 -03

--- actualizar.sh
+++ actualizar.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 TIMEFORMAT='%3U'
-cd /home/waltermas/MEGAsync/scripts/infomesas-gui/mobile
+cd /home/waltermas/MEGAsync/infomesas-gui/mobile
 echo -n "generar pendientes mobile "
-time poetry run python m_generar_pendientes_jinja.py 
+python m_generar_pendientes_jinja.py 
 echo -n "generar cada cliente mobile "
-time poetry run python m_generar_cada_cliente_jinja.py
+time python m_generar_cada_cliente_jinja.py
 # echo -n "generar todos los pedidos mobile "
 # time poetry run python m_generar_todos_pedidos_jinja.py
 echo -n "generar soldini "
-time poetry run python m_generar_soldini_jinja.py
+time python m_generar_soldini_jinja.py
 
-cd /home/waltermas/MEGAsync/scripts/infomesas-gui
+cd /home/waltermas/MEGAsync/infomesas-gui
 echo -n "generar cheques "
-poetry run python generar_cheques_jinja.py
+python generar_cheques_jinja.py
 echo -n "generar precios "
-poetry run python generar_precios_jinja.py
+python generar_precios_jinja.py
 
 # poetry run python generar_deudas_jinja.py
 # poetry run python generar_cc_jinja.py
