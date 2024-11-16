c7860391bd4e7bed5987807da920dcb6f8301247
waltermas <walterjmas@gmail.com>
2024-11-16 18:57:21 -0300

    sáb 16 nov 2024 18:57:21 -03

--- actualizar.sh
+++ actualizar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TIMEFORMAT='%3U'
-cd /home/waltermas/MEGA/infomesas-gui/mobile
+cd /home/walter/infomesas-gui/mobile
 python m_generar_clientes_jinja.py
 python m_generar_pendientes_jinja.py
 python m_generar_cada_cliente_jinja.py
